e3a32ae81aae61e198c0db31e6907c8c53653dc4
b0nk <fsantos.pt@hotmail.com>
2022-12-12 00:32:08 +0000

    day08 - part2 & cleanup

--- day08.c
+++ day08.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAP_SIZE 99
 
 
-int is_visible(int t_map[MAP_SIZE][MAP_SIZE], int x, int y){
+int is_visible_with_score(int t_map[MAP_SIZE][MAP_SIZE], int x, int y, int* score){
 	int tree = t_map[x][y];
 	int hidden_directions = 0;
+	int current_score, north_score, south_score, west_score, east_score;
 
-	for(int i = x - 1; i >= 0; i--){
+	for(int i = x - 1, j = 1; i >= 0; i--, j++){
+		north_score = j;
 		if(tree <= t_map[i][y]){
 			hidden_directions++;
 			break;
 		}
 	}
-	for(int i = x + 1; i < MAP_SIZE; i++){
+	for(int i = x + 1, j = 1; i < MAP_SIZE; i++, j++){
+		south_score = j;
 		if(tree <= t_map[i][y]){
 			hidden_directions++;
 			break;
 		}
 	}
-	for(int i = y - 1; i >= 0; i--){
+	for(int i = y - 1, j = 1; i >= 0; i--, j++){
+		west_score = j;
 		if(tree <= t_map[x][i]){
 			hidden_directions++;
 			break;
 		}
 	}
-	for(int i = y + 1; i < MAP_SIZE; i++){
+	for(int i = y + 1, j = 1; i < MAP_SIZE; i++, j++){
+		east_score = j;
 		if(tree <= t_map[x][i]){
 			hidden_directions++;
 			break;
 		}
+	}
+
+	current_score = north_score * south_score * east_score * west_score;
+	if(current_score > *score){
+		*score = current_score;
 	}
 	if(hidden_directions < 4){
 		return 1;
@@ -39,10 +48,10 @@ int is_visible(int t_map[MAP_SIZE][MAP_SIZE], int x, int y){
 	return 0;
 }
 
-void count_visible_trees(int t_map[MAP_SIZE][MAP_SIZE], int* v){
+void count_visible_trees_and_score(int t_map[MAP_SIZE][MAP_SIZE], int* v, int* score){
 	for(int i = 1; i < MAP_SIZE - 1; i++){
 		for(int j = 1; j < MAP_SIZE - 1; j++){
-			if(is_visible(t_map, i, j)){
+			if(is_visible_with_score(t_map, i, j, score)){
 				*v += 1;
 			}
 		}
@@ -61,8 +70,7 @@ int main(int argc, char* argv){
 	FILE* fp = fopen(filename, "r");
 
 	while(getline(&line, &len, fp) != -1) {
-		int length = strlen(line);
-		for(int i = 0; i < length; i++){
+		for(int i = 0; i < MAP_SIZE; i++){
 			tree_map[l][i] = line[i] - '0';
 		}
 		l++;
@@ -72,10 +80,12 @@ int main(int argc, char* argv){
 	free(line);
 
 	int visible = MAP_SIZE * 4 - 4;
+	int high_score = 1;
 
-	count_visible_trees(tree_map, &visible);
+	count_visible_trees_and_score(tree_map, &visible, &high_score);
 
 	printf("part1: %d\n", visible);
+	printf("part2: %d\n", high_score);
 
 	return 0;
 }
