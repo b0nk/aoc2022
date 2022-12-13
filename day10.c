02454b3cbe54fe0e345b9d1fd36ea7c3c7a80baf
b0nk <fsantos.pt@hotmail.com>
2022-12-13 21:12:15 +0000

    day10 - part2

--- day10.c
+++ day10.c
@@ -3,13 +3,28 @@
 #include <string.h>
 
 #define STRENGTH_SIZE 6
+#define WIDTH 40
 
-void increment_cycle(int* cycle, int* rax, int* next_print, int* strength_list[], int* count){
+void increment_cycle_and_print(int* cycle, int* rax, int* next_strength, int* line, int* strength_list[], int* count){
+	
 	*cycle += 1;
-	if(*cycle == *next_print){
+	if(*cycle < WIDTH * *line){
+		if(*rax == *cycle % WIDTH || *rax + 1 == *cycle % WIDTH || *rax + 2 == *cycle % WIDTH){
+			printf("#");
+		}
+		else{
+			printf(".");
+		}
+	}
+	else{
+		printf("\n");
+		*line += 1;
+	}
+	
+	if(*cycle == *next_strength){
 		*strength_list[*count] = *cycle * *rax;
 		*count += 1;
-		*next_print += 40;
+		*next_strength += WIDTH;
 	}
 }
 
@@ -18,10 +33,11 @@ int main(int argc, char* argv){
 	int rax = 1;
 	int arg;
 	int cycle = 0;
-	int next_print = 20;
+	int next_strength = 20;
+	int line_n = 1;
 	int strength_count = 0;
 	int* strength_list[STRENGTH_SIZE];
-
+	
 	for(int i = 0; i < STRENGTH_SIZE; i++){
 		strength_list[i] = malloc(sizeof(int));
 	}
@@ -32,13 +48,15 @@ int main(int argc, char* argv){
 	char* filename = "inputday10";
 	FILE* fp = fopen(filename, "r");
 
+	//printf("part2: ");
+
 	while(getline(&line, &len, fp) != -1) {
 		if(line[0] == 'n'){
-			increment_cycle(&cycle, &rax, &next_print, strength_list, &strength_count);
+			increment_cycle_and_print(&cycle, &rax, &next_strength, &line_n, strength_list, &strength_count);
 		}
 		else{
-			increment_cycle(&cycle, &rax, &next_print, strength_list, &strength_count);
-			increment_cycle(&cycle, &rax, &next_print, strength_list, &strength_count);
+			increment_cycle_and_print(&cycle, &rax, &next_strength, &line_n, strength_list, &strength_count);
+			increment_cycle_and_print(&cycle, &rax, &next_strength, &line_n, strength_list, &strength_count);
 			arg = atoi(&line[4]);
 			rax += arg;
 		}
